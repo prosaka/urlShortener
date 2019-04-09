ca5510a6b62711250e0ba2f16efb0a09516faa4f
Roberto Guerra <prosaka@hotmail.com>
2019-04-08 23:38:06 -0300

    Delete config.js

--- config/config.js
+++
@@ -1,4 +0,0 @@
-const connect_db = 'mongodb+srv://roberto_admin:2KiELZdFtn4BHNm!@db-qwguk.mongodb.net/test?retryWrites=true';
-const db_opt = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
-
-module.exports = { connect_db, db_opt }
