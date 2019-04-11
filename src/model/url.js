ce10981caaf00c249946fe1eea0cf498ad2e8c61
Roberto <prosaka@hotmail.com>
2019-04-11 00:08:23 -0300

    (fix): set url

--- src/model/url.js
+++ src/model/url.js
@@ -13,7 +13,7 @@ const urlSchema = new Schema({
 urlSchema.pre('save', async function(next) {
     let url = this;
     url.short = await url.short.replace(/([^A-Za-z0-9])/, '').slice(1,6);
-    url.shortUrl = await `${location.href}/${url.short}`  
+    url.shortUrl = await `${process.env.URL}/${url.short}`  
     return next()
 })
 
