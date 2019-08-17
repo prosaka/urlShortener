0a14eb8f3fcf87c4710a8d8e2a0f2ea8b39b403d
Roberto Guerra <prosaka@hotmail.com>
2019-08-17 10:20:08 -0300

    Task - change url data base

--- config/config.js
+++ config/config.js
@@ -1,4 +1,10 @@
-const connect_db = 'mongodb+srv://omnistack:hxZTJsbecMtMvt4Y@db-qwguk.mongodb.net/test?retryWrites=true';
-const db_opt = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+const connect_db =
+  "mongodb+srv://write:write@cluster0-zanqa.mongodb.net/test?retryWrites=true&w=majority";
+const db_opt = {
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  poolSize: 5,
+  useNewUrlParser: true
+};
 
-module.exports = { connect_db, db_opt } 
+module.exports = { connect_db, db_opt };
